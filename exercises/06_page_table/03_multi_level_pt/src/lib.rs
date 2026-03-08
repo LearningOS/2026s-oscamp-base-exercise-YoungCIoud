927dfd3a1055d1b5795a48185369cfcc60076d54
YoungCIoud <YOUNG.050330.cdut@outlook.com>
2026-03-08 22:55:20 +0800

    完成了多级页表的练习

--- exercises/06_page_table/03_multi_level_pt/src/lib.rs
+++ exercises/06_page_table/03_multi_level_pt/src/lib.rs
@@ -94,6 +94,23 @@ impl Sv39PageTable {
         ppn
     }
 
+    /// 在 ppn 对应的页表中取出第 vpn 个页表项
+    fn get_pte(&self, ppn: u64, vpn: usize) -> Option<u64> {
+        if let Some(tb) = self.nodes.get(&ppn) {
+            Some(tb.entries[vpn])
+        } else {
+            None
+        }
+    }
+
+    fn get_pte_mut(&mut self, ppn: u64, vpn: usize) -> Option<&mut u64> {
+        if let Some(tb) = self.nodes.get_mut(&ppn) {
+            Some(&mut tb.entries[vpn])
+        } else {
+            None
+        }
+    }
+
     /// 从 39 位虚拟地址中提取第 `level` 级的 VPN。
     ///
     /// - level=2: 取 bits [38:30]
@@ -102,8 +119,7 @@ impl Sv39PageTable {
     ///
     /// 提示：右移 (12 + level * 9) 位，然后与 0x1FF 做掩码。
     pub fn extract_vpn(va: u64, level: usize) -> usize {
-        // TODO: 从虚拟地址中提取指定级别的 VPN 索引
-        todo!()
+        ((va >> 12 + level * 9) as usize) & 0x1FF
     }
 
     /// 建立从虚拟页到物理页的映射（4KB 页）。
@@ -113,13 +129,28 @@ impl Sv39PageTable {
     /// - `pa`: 物理地址（会自动对齐到页边界）
     /// - `flags`: 标志位（如 PTE_V | PTE_R | PTE_W）
     pub fn map_page(&mut self, va: u64, pa: u64, flags: u64) {
-        // TODO: 实现三级页表的映射
-        //
-        // 提示：你需要从根页表开始，逐级向下遍历页表层级（level 2 → level 1 → level 0）。
-        // 对于中间层级（level 2 和 level 1），如果对应 VPN 的页表项（PTE）无效（PTE_V == 0），
-        // 则需要分配一个新的页表节点（使用 alloc_node），并将新节点的 PPN 写入当前 PTE（仅设置 PTE_V 标志）。
-        // 最后在 level 0 的 PTE 中写入目标物理页号（pa >> 12）和 flags。
-        todo!()
+        // // TODO: 实现三级页表的映射
+        // //
+        // // 提示：你需要从根页表开始，逐级向下遍历页表层级（level 2 → level 1 → level 0）。
+        // // 对于中间层级（level 2 和 level 1），如果对应 VPN 的页表项（PTE）无效（PTE_V == 0），
+        // // 则需要分配一个新的页表节点（使用 alloc_node），并将新节点的 PPN 写入当前 PTE（仅设置 PTE_V 标志）。
+        // // 最后在 level 0 的 PTE 中写入目标物理页号（pa >> 12）和 flags。
+        // todo!()
+        let mut cur_ppn = self.root_ppn;
+        for level in (0..3).rev() {
+            let vpn = Self::extract_vpn(va, level);
+            if level == 0 {
+                *self.get_pte_mut(cur_ppn, vpn).unwrap() = (pa >> 12 << 10) | flags;
+            } else {
+                if self.get_pte(cur_ppn, vpn).unwrap() & PTE_V == 0 {
+                    *self.get_pte_mut(cur_ppn, vpn).unwrap() = (self.next_ppn << 10) | 1;
+                    self.nodes.insert(self.next_ppn, PageTableNode::new());
+                    self.next_ppn += 1;
+                }
+
+                cur_ppn = self.get_pte(cur_ppn, vpn).unwrap() >> 10;
+            }
+        }
     }
 
     /// 遍历三级页表，将虚拟地址翻译为物理地址。
@@ -141,7 +172,26 @@ impl Sv39PageTable {
         // 如果 PTE 是叶节点（即 R、W、X 标志位中有至少一个被置位），则可以直接使用该 PTE 中的物理页号（PPN）计算最终的物理地址。
         // 否则，该 PTE 指向下一级页表节点，继续遍历下一级。
         // 遍历到 level 0 时，PTE 必须是叶节点。
-        todo!()
+        // todo!()
+        let mut cur_ppn = self.root_ppn;
+        for level in (0..3).rev() {
+            let vpn = Self::extract_vpn(va, level);
+            let pte = self.get_pte(cur_ppn, vpn).unwrap();
+            if pte & PTE_V == 0 {
+                return TranslateResult::PageFault;
+            }
+
+            if pte & (PTE_R | PTE_W | PTE_X) != 0 {
+                let ppn = pte >> 10;
+                let shift = if level == 0 { 12 } else { 21 };
+                let pa = (ppn << 12) | (va & ((1 << shift) - 1));
+                return TranslateResult::Ok(pa);
+            } else {
+                cur_ppn = pte >> 10;
+            }
+        }
+
+        TranslateResult::PageFault
     }
 
     /// 建立大页映射（2MB superpage，在 level 1 设叶子 PTE）。
@@ -160,7 +210,21 @@ impl Sv39PageTable {
         // 你需要在 level 2 找到或创建中间页表节点，然后在 level 1 写入叶子 PTE。
         // 注意大页的物理页号计算方式与普通页相同（pa >> 12），
         // 但翻译时 offset 包含虚拟地址的低 21 位（VPN[0] 部分 + 12 位页内偏移）。
-        todo!()
+        let mut cur_ppn = self.root_ppn;
+        for level in (1..3).rev() {
+            let vpn = Self::extract_vpn(va, level);
+            if level == 1 {
+                *self.get_pte_mut(cur_ppn, vpn).unwrap() = (pa >> 12 << 10) | flags;
+            } else {
+                if self.get_pte(cur_ppn, vpn).unwrap() & PTE_V == 0 {
+                    *self.get_pte_mut(cur_ppn, vpn).unwrap() = (self.next_ppn << 10) | 1;
+                    self.nodes.insert(self.next_ppn, PageTableNode::new());
+                    self.next_ppn += 1;
+                }
+
+                cur_ppn = self.get_pte(cur_ppn, vpn).unwrap() >> 10;
+            }
+        }
     }
 }
 
